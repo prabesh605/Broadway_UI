d821dada818e3e70c31ba467d651b6e318c5d155
Prabesh Bhattarai <prabesh605@gmail.com>
2025-12-16 09:11:11 +0545

    worked on current location

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,5 +1,6 @@
 import 'package:broadway_example_ui/weather_service.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
@@ -13,36 +14,87 @@ class _WeatherScreenState extends State<WeatherScreen> {
   double temp = 0.0;
   int humidity = 0;
   double speed = 0.0;
-  String cityName = '';
-  //searchController
+  // String cityName = '';
+  String city = "";
+  final searchController = TextEditingController();
+
+  Future<Position> determinePosition() async {
+    bool serviceEnabled; //hamro loction is enable or not
+    LocationPermission permission; // hamile permission diyeko xa ki xaina
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    permission = await Geolocator.checkPermission();
+    if (!serviceEnabled) {
+      return Future.error("Location is not enable");
+    }
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location permission is denied");
+      }
+    }
+    return await Geolocator.getCurrentPosition();
+
+    // location is on or off
+    //user le permission diyeko xa ki xaina
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Weather App")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final Position position = await determinePosition();
+          double latitude = position.latitude;
+          double longitude = position.longitude;
+          print("Latitude: ${position.latitude}");
+          print("Longitude: ${position.longitude}");
+          final data = await service.getCurrentLocationWeather(
+            latitude,
+            longitude,
+          );
+          print(data);
+        },
+        child: Icon(Icons.my_location),
+      ),
       body: Column(
         children: [
           //TextFormField
           //controller: searchController
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: searchController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text("Enter city Name"),
+              ),
+            ),
+          ),
           ElevatedButton(
             onPressed: () async {
-              //String cityname =controller
-              if (cityName.isNotEmpty) {
-                final data = await service.getWeather(cityName);
+              String cityname = searchController.text;
+              if (cityname.isNotEmpty) {
+                final data = await service.getWeather(cityname);
                 print(data);
 
                 setState(() {
                   temp = data.main.temp;
                   humidity = data.main.humidity;
                   speed = data.wind.speed;
+                  city = data.name;
                 });
                 print(temp);
+              } else {
+                print("Enter City Name");
               }
 
               // temp= data
             },
             child: Text("Get Weather"),
           ),
+          Text("Your City is $city"),
           Text("Your Temperature is $temp"),
           Text("Your Humidity is $humidity"),
           Text("Your Speed is $speed"),
--- lib/weather_service.dart
+++ lib/weather_service.dart
@@ -7,14 +7,42 @@ class WeatherService {
   final String apiKey = "0b10461d5a30e6612ce19d84c97ef651";
   //city name --string
   Future<WeatherModel> getWeather(String cityName) async {
-    final response = await http.get(
-      Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey",
-      ),
-    );
-    final jsonData = jsonDecode(response.body);
-    WeatherModel weatherModel = WeatherModel.fromJson(jsonData);
+    try {
+      final response = await http.get(
+        Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey",
+        ),
+      );
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+        WeatherModel weatherModel = WeatherModel.fromJson(jsonData);
+        return weatherModel;
+      } else {
+        throw Exception("Failed to load weather");
+      }
 
-    return weatherModel;
+      // return weatherModel;
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+
+  Future<WeatherModel> getCurrentLocationWeather(double lat, double lon) async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey",
+        ),
+      );
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+        WeatherModel weatherModel = WeatherModel.fromJson(jsonData);
+        return weatherModel;
+      } else {
+        throw Exception("Failed to load weather");
+      }
+    } catch (e) {
+      throw e.toString();
+    }
   }
 }
